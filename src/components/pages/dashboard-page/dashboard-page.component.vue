d2aeaa5e4476b2c65ac9c5e97cc507eeab20036e
Fangyun,Liu <liu00433@algonquinlive.com>
2019-02-14 12:35:59 -0500

    save it

---
+++ src/components/pages/dashboard-page/dashboard-page.component.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="DashboardPage">
+    Dashboard Page Boom
+  </div>
+</template>
+
+<script src="./dashboard-page.component.js"></script>
+<style src="./dashboard-page.component.css"></style>
+
